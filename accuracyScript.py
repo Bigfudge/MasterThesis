44dfed65a4965a9681d01552ab196742a3db08ac
Simon Persson <simonpersson@flaskpost.org>
2018-12-12 17:10:44 +0100

    Started work

---
+++ accuracyScript.py
@@ -0,0 +1,17 @@
+from subprocess import call
+import os
+
+def	charAcc(genPath, truthPath):
+	pairOfPaths= []
+	for	filename in os.listdir(genPath):
+		testList.append([genPath+filename])
+	for filename 
+
+def main():
+	ocropusArgus= "/chalmers/users/simp/MasterThesis/OCR output/Ocropus/Argus/"
+	charAcc("/chalmers/users/simp/MasterThesis/OCR output/Ocropus/Argus/", "hej")
+	#call(["ls", "-l"])
+
+	
+
+main()
