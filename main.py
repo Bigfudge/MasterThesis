a875589f54c7086d66c6bae5e1fda3fa11c2267a
Simon Persson <simonpersson@flaskpost.org>
2019-01-29 16:42:39 +0100

    Added a daily log

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
+import csv
+import os
+import glob
+
+#Kanske skita i denna???
+def prune_word(word):
+	if(not word[len(word)-1].isalnum()):
+		return(word[:-1])
+	return(word)
+
+def contains_non_alfanum(word):
+	errors = list(filter(lambda a: not (a.isalnum() | (a in {'-','å','ä','ö'})), word[:-1]))
+	return len(errors)
+def get_bigram_freq(word):
+	return 1
+def get_frequency(word):
+	return 1
+def is_actuall_word(word, truth_file):
+	#Returns 1 if 'word' is represented in 'truth_file', otherwise 0
+	return 0
+
+def createCSV(input_dir, output_filename):
+	for file in os.listdir(input_dir):
+		truth = open(input_dir+file)
+		words = [word for line in truth for word in line.split()]
+		csvData = [[]]
+		for word in words:
+			#ToDo: Check for duplicates, and only add if unique
+			csvData.append([prune_word(word),
+							contains_non_alfanum(word),
+							get_bigram_freq(word),
+							get_frequency(word),
+							1])
+§
+		with open(output_filename, 'a') as csvFile:
+		    writer = csv.writer(csvFile)
+		    writer.writerows(csvData)
+
+		csvFile.close()
+		truth.close()
 
 def main():
-	ocr = open('ocr_test1.txt')
-	truth= open('truth_test1.txt')
+	os.remove('input_vector.csv')
+	createCSV('./Evaluation-script/ManuelTranscript/Argus/', 'input_vector.csv')
+	#createCSV('./Evaluation-script/ManuelTranscript/Grepect/', 'input_vector.csv')
 
-	words = [word for line in ocr for word in line.split()]
-	
-	for word in words:
-		test = list(filter(lambda a: not (a.isalnum() | (a in {'å','ä','ö'})), word[:-1]))
-		if len(test) > 0:
-			print(word)
-
-	ocr.close()
-	truth.close()
 
 main()
